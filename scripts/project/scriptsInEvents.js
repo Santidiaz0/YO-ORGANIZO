98714cc0fe247a5bc4cd7db6a2fddd9ef4d0769b
Santidiaz0 <83663324+Santidiaz0@users.noreply.github.com>
2021-08-26 19:43:17 -0500

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+		async HojaDeEventos1_Event17_Act1(runtime, localVars)
+		{
+			Alert(" Bienvenido a ciudad Alegria. Toca el panel solar si quieres mover el auto, tambien puedes ir a pie. Te recomiendo visitar el zoologico, estación de policia y el veterinario.")
+		}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
